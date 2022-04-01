4dbb25f275bcc5426a50ff286cb1aacce9ed13dd
mjsdev5 <mcbridejames196@gmail.com>
2022-04-01 18:29:18 +0300

    building the ride options card component

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,16 +1,37 @@
+import { createStackNavigator } from "@react-navigation/stack";
 import React from "react";
 import { StyleSheet, View } from "react-native";
 import tw from "twrnc";
 import Map from "../components/Map";
+import NavigateCard from "../components/NavigateCard";
+import RideOptionsCard from "../components/RideOptionsCard";
 
 const MapScreen = () => {
+  const Stack = createStackNavigator();
   return (
     <View>
       <View style={tw`h-1/2`}>
         <Map />
       </View>
 
-      <View sttle={tw`h-1/2`}></View>
+      <View sttle={tw`h-1/2`}>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="NavigateCard"
+            component={NavigateCard}
+            options={{
+              headerShown: false,
+            }}
+          />
+          <Stack.Screen
+            name="RideOptionsCard"
+            component={RideOptionsCard}
+            options={{
+              headerShown: false,
+            }}
+          />
+        </Stack.Navigator>
+      </View>
     </View>
   );
 };
