6df96803ecc26943a040e41c1a913e317644b774
mjsdev5 <mcbridejames196@gmail.com>
2022-04-01 16:17:39 +0300

    editing homescreen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,5 +1,6 @@
 import { Image, SafeAreaView, StyleSheet, View } from "react-native";
 import tw from "twrnc";
+import NavOptions from "../components/NavOptions";
 
 const HomeScreen = () => {
   return (
@@ -9,6 +10,7 @@ const HomeScreen = () => {
           source={{ uri: "https://links.papareact.com/gzs" }}
           style={{ width: 100, height: 100, resizeMode: "contain" }}
         />
+        <NavOptions />
       </View>
     </SafeAreaView>
   );
