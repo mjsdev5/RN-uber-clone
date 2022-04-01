d7f14750733023827d6c2d8b86e06200d3fd1d97
mjsdev5 <mcbridejames196@gmail.com>
2022-04-01 14:47:59 +0300

    building the homescreen component & installing tailwind

---
+++ screens/HomeScreen.js
@@ -0,0 +1,23 @@
+import { Image, SafeAreaView, StyleSheet, View } from "react-native";
+import tw from "twrnc";
+
+const HomeScreen = () => {
+  return (
+    <SafeAreaView style={tw`bg-white h-full`}>
+      <View style={tw`p-5`}>
+        <Image
+          source={{ uri: "https://links.papareact.com/gzs" }}
+          style={{ width: 100, height: 100, resizeMode: "contain" }}
+        />
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default HomeScreen;
+
+const styles = StyleSheet.create({
+  text: {
+    color: "blue",
+  },
+});
