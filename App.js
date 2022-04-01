ead8e2402b33651f273ec5f3de4618c4449efc7a
mjsdev5 <mcbridejames196@gmail.com>
2022-04-01 16:32:20 +0300

    building navoptions

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import { StyleSheet } from "react-native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 import { Provider } from "react-redux";
 import HomeScreen from "./screens/HomeScreen";
 import { store } from "./store";
@@ -6,7 +7,9 @@ import { store } from "./store";
 export default function App() {
   return (
     <Provider store={store}>
-      <HomeScreen />
+      <SafeAreaProvider>
+        <HomeScreen />
+      </SafeAreaProvider>
     </Provider>
   );
 }
--- components/NavOptions.js
+++ components/NavOptions.js
@@ -1,3 +1,4 @@
+import { Icon } from "@rneui/base";
 import React from "react";
 import { FlatList, Image, Text, TouchableOpacity, View } from "react-native";
 import tw from "twrnc";
@@ -31,6 +32,12 @@ const NavOptions = () => {
               source={{ uri: item.image }}
             />
             <Text style={tw`mt-2 text-lg font-semibold`}>{item.title}</Text>
+            <Icon
+              style={tw`p-2 bg-black rounded-full w-10 mt-4`}
+              type="antdesign"
+              name="arrowright"
+              color="white"
+            />
           </View>
         </TouchableOpacity>
       )}
